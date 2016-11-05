636b00b1ff47f661759853c41bea3da6bf3db385
Tim Vögtli <tim.voegtli@gibmit.ch>
2016-11-05 16:33:31 +0100

    Init Repo

---
+++ HelloWorld/src/HelloWorld.cpp
@@ -0,0 +1,19 @@
+/*
+ * HelloWorld.cpp
+ *
+ * HelloWorld is just another hello world program.
+ * It is a project to improve my c++ skills.
+ *
+ * Created on: 05.11.2016
+ * Author: Tim Vögtli
+ */
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+	cout << "Hello World" << endl;
+
+	return 0;
+}
