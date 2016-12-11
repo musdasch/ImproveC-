ceb90e53db25fcd532a6d5556b3ed5548f90261a
Tim Vögtli <tim.voegtli@gibmit.ch>
2016-12-11 11:21:57 +0100

    Add some comments for explanation

--- Exactly/src/exactly.cpp
+++ Exactly/src/exactly.cpp
@@ -1,7 +1,9 @@
 /*
  * exactly.cpp
  *
- * This program is a example for accuracy with float numbers.
+ * This program is a example for accuracy with float numbers. The results s1 and s2 should be the same
+ * but computer arithmetic has some inaccuracies. for improvement of the accuracy
+ * you have to order the sums and add the smallest number first.
  *
  * Created on: 11.12.2016
  * Author: Tim Vögtli
@@ -11,14 +13,18 @@
 using namespace std;
 
 int main(){
+	//Set the variables
 	float a = 1.234567E-7, b = 1.000000, c = -b, s1, s2;
 
+	//Calculate the sum 1
 	s1 = a + b;
 	s1 += c;
 
+	//Calculate the sum 2
 	s2 = a;
 	s2 += b + c;
 
+	//Discripton
 	cout << "a = " << a << ", b = " << b << ", c = " << c << endl;
 	cout << "Sum 1 = " << a << " + " << b << endl;
 	cout << "Sum 1 = Sum 1 + " << c << endl;
