fc1430af1b90b771dd0277132f52391dedf29059
Tim Vögtli <tim.voegtli@gibmit.ch>
2016-12-12 21:41:38 +0100

    Add a program that displays the fibonacci sequence

---
+++ Fibonacci/src/fibonacci.cpp
@@ -0,0 +1,36 @@
+/*
+ * fibonacci.cpp
+ *
+ * The Fibonacci program displays the fibonacci sequence.It starts with a chosen number end ends after a defined length.
+ * First you define the number to start with after you define the number of passes.
+ *
+ * Created on: 11.12.2016
+ * Author: Tim Vögtli
+ */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	long sum, a, temp, count;
+
+	cout << "Number to start with: ";
+	cin >> a;
+
+	cout << endl << "Number of Passes: ";
+	cin >> count;
+	cout << endl;
+
+	sum = a * 2;
+	cout << a << " + " << a << " = " << sum << endl;
+
+	for( long i = 0; i < count; i++ ){
+		cout << a << " + " << sum << " = ";
+		temp = a + sum;
+		a = sum;
+		sum = temp;
+		cout << sum << endl;
+	}
+
+	return 0;
+}
