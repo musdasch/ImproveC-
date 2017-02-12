239766449ba790cc3b8744e8e4fb2ebea9d6fa49
Tim Vögtli <tim.voegtli@gibmit.ch>
2017-02-12 11:13:24 +0100

    Add comments to ForLoop

--- ForLoop/src/ForLoop.cpp
+++ ForLoop/src/ForLoop.cpp
@@ -7,18 +7,38 @@
  * Author: Tim Vögtli
  */
 
+/**
+ * The include of the iostream library.
+ */
 #include<iostream>
 
+/**
+ * use the namespace std so that you haven't to typed std::
+ */
 using namespace std;
 
 int main(){
 
+	/**
+	 * This is the input Variable.
+	 */
 	int input = 0;
 
+	/**
+	 * It is used to input a number for the for loop.
+	 */
 	cout << "Count: ";
 	cin >> input;
 
+	/**
+	 * The for loop.
+	 */
 	for( int i = 0; i < input; i++ ){
+
+		/**
+		 * counts from 0 to the number the user have given.
+		 */
 		cout << "Count: " << i << endl;
+
 	}
 }
